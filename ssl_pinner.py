8599d4bfb4e262770db536b584ef65e22186d47c
yayip <39186995+yayip@users.noreply.github.com>
2020-10-01 16:57:25 +0700

    Add files via upload

---
+++ ssl_pinner.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+
+from optparse import OptionParser
+import os, sys
+
+usage = sys.argv[0] + " Options "
+parser = OptionParser(usage)
+parser.add_option("-d","--der", dest="der", default=False, metavar="der", help="DER Cerfiticate")
+parser.add_option("-p","--pem", dest="pem", default=False, metavar="pem", help="PEM Cerfiticate")
+(options, args) = parser.parse_args()
+
+def pinner(cert):
+	os.system("openssl x509 -in " + cert + " -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64")
+
+try:
+	if __name__ == '__main__':
+		if options.der != True and options.pem != True:
+			if options.der:
+				os.system("mkdir tmp")
+				os.system("openssl x509 -inform der -in " + options.der + " -out tmp/cert.pem -pubkey")
+				pinner("tmp/cert.pem")
+				os.system("rm -rf tmp")
+			if options.pem:
+				pinner(options.pem)
+		else:
+			print (sys.argv[0] + " --help")
+except:
+	pass
+
+	
